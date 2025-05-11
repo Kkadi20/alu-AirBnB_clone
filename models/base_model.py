90307d4e481d678c5ad702f665b022faf018278d
Kkadi20 <k.koita@alustudent.com>
2025-05-11 18:03:16 +0200

    models and model tests

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
-    """Base model for all classes in the AirBnB clone project"""
-
-    def __init__(self):
-        """Initialize a new instance with id and timestamps"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-    def __str__(self):
-        """String representation of the BaseModel instance"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel or from dictionary"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def save(self):
-        """Updates the updated_at attribute with current time"""
+        """Update updated_at and save the object to file"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary of all attributes, with ISO datetime format"""
+        """Return dictionary with ISO date strings + class name"""
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
         return obj_dict
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
